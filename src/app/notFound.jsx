19602b5680012bc5ba5b6cd20f5f9914e45440a9
anas0723 <anassoomro23@gmail.com>
2025-06-16 12:53:03 +0500

    Start working on figma target is complete this porject in 4 dayu

---
+++ src/app/notFound.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const notFound = () => {
+  return (
+    <>
+      <h1>Page Not Found</h1>
+      <p>Sorry, the page you are looking for does not exist.</p>
+    </>
+  )
+}
+
+export default notFound
