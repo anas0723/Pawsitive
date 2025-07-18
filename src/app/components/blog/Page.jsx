72582b138d13f9f2af65949add318fdd46a52fc1
Muhammad Anas Soomro <anassoomro23@gmail.com>
2025-07-18 16:37:34 +0500

    deployment

--- src/app/components/blog/Page.jsx
+++ src/app/components/blog/Page.jsx
@@ -4,7 +4,7 @@ import BlogList from './BlogList';
 export default function BlogPage() {
   return (
     <main className="bg-white min-h-screen py-6 px-2 sm:px-4">
-      <div className="max-w-7xl mx-auto w-full px-4">
+      <div className="max-w-7xl mx-auto w-full   px-5">
         <BlogHero />
         <BlogList />
       </div>
